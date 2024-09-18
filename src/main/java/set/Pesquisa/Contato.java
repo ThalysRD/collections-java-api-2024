af06ab6a14e91a0dda1876cac944314dfb48e40f
ThalysRD <tgbr66@gmail.com>
2024-09-18 19:52:27 -0300

    Feat: Implementa as classes Contato e AgendaContatos para práticar pesquisa usando set e hashset

---
+++ src/main/java/set/Pesquisa/Contato.java
@@ -0,0 +1,39 @@
+package main.java.set.Pesquisa;
+
+import java.util.Objects;
+
+public class Contato {
+    private String nome;
+    private int numero;
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNumero(int novoNumero) {
+        this.numero = novoNumero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(getNome(), contato.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Contato: " + nome + " Número: " + numero;
+    }
+}
